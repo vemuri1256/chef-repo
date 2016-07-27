13df3a016a4eae4d13c3a93f87c4bcf9c8e30dee
vemuri <pitchaiahchowdary.vemuri@capitalone.com>
2016-07-26 22:58:05 -0500

    rearrange the structure

---
+++ cookbooks/test_devops/README.md
@@ -0,0 +1,7 @@
+Splunk Cookbook
+================
+Provides required scripts to configure Splunk integration for an EC2 instance to configure correct host name of the instance.
+
+
+```text
+Copyright 2015, Capital One, Inc.
---
+++ cookbooks/test_devops/metadata.rb
@@ -0,0 +1,7 @@
+name             'test_devops'
+maintainer       'vemuri'
+maintainer_email 'vemuri1256@gmail.com'
+license          ''
+description      'hello world  cookbook with nginx'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
