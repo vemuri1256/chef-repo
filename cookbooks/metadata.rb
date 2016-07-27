1fd8f477fc0a9f48a5b3660cc982ec283572a4c6
chowdary Vemuri <vemuri1256@gmail.com>
2016-07-26 22:59:36 -0500

    Delete metadata.rb

--- cookbooks/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'test_devops'
-maintainer       'vemuri'
-maintainer_email 'vemuri1256@gmail.com'
-license          ''
-description      'hello world  cookbook with nginx'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
